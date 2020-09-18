b62003f5e047a1f83b1e4a63399b572d403662f4
Inc1ementia <71116508+Inc1ementia@users.noreply.github.com>
2020-09-18 07:32:02 +0200

    Add files via upload

---
+++ kNN.R
@@ -0,0 +1,85 @@
+eucDist <- function(u,v) {  #функция расстояния между парой точек
+  return (sqrt(sum((u-v)^2)))
+}
+colors <- c("setosa"="pink1","versicolor"="gold","virginica"="skyblue1")
+sortObjbyDist <- function(xl,z,metricFunc=eucDist) {  #функция сортировки массива по расстоянию до z
+  l <- dim(xl)[1]
+  n <- dim(xl)[2]-1
+  dist <- matrix(NA,l,2)
+  for (i in 1:l) {
+    dist[i, ] <- c(i, metricFunc(xl[i,1:n],z))  #создание списка пар (номер объекта, расстояние до z)
+  }
+  orderedXl <- xl[order(dist[ ,2]), ]   #сортировка списка объектов
+  return (orderedXl)
+}
+kNN <- function(xl,z,k) {   #функция выбора класса методов kNN
+  orderedXl <- sortObjbyDist(xl,z)
+  n <- dim(orderedXl)[2]-1
+  classes <- orderedXl[1:k,n+1]   #получает список видов для ближайших k объектов
+  counts <- table(classes)   #строить из них таблицу количество цветов каждого вида
+  class <- names(which.max(counts))   #выбирает тот вид, у которого больше всего представителей
+  return (class)
+}
+determKNN_LOO <- function(xl) {   #определение оптимального k методом LOO
+  l <- dim(xl)[1]
+  n <- dim(xl)[2]-1
+  maxK <- l-1
+  errorForK <- matrix(0.0,maxK,1)
+  for (i in 1:l) {  #перебираем каждый элемент
+    obj <- xl[i, ]
+    newXl <- xl[-i, ]  #убираем его из выборки
+    z <- obj[1:n]
+    orderedXl <- sortObjbyDist(newXl,z)  #находим расстояния до всех объектов
+    classes <- c("setosa"=0,"versicolor"=0,"virginica"=0)
+    print(i)
+    for (k in 1:maxK) {   #перебираем значение k
+      classes[orderedXl[k,n+1]] <- classes[orderedXl[k,n+1]]+1  #добавляем k-ый элемент в свою группу
+      class <- names(which.max(classes))  #выбирает тот вид, у которого больше всего представителей
+      if (class!=obj[n+1]) {   #если произошла ошибка классификации
+        errorForK[k]=errorForK[k]+1   #то увиличиваем для данного k значение ошибки
+      }
+    }
+  }
+  LOO <- matrix(NA,maxK,2)
+  for (k in 1:maxK) {
+    LOO[k, ] <- c(k,errorForK[k])   #строим массив пар (номер, число ошибок)
+  }
+  optK <- which.min(errorForK)  #выбираем оптимальное k
+  plot(LOO,pch=1,type="l",col="blue",xlab="k",ylab="LOO(k)",main="Find optimal value of k for kNN with LOO-algo")
+  points(optK,LOO[optK,2],pch=21,bg="red",col="red")
+  res <- paste("optK=",optK)   #готовит подпись для графика
+  text(optK,LOO[optK,2]+7,labels=res)
+  return (optK)
+}
+main <- function() {
+  parOne <- 3   #чтобы можно было легко поменять параметры
+  parTwo <- 4
+  titles <- dimnames(iris)[2]
+  xname <- titles[[1]][parOne]   #названия для подписей на карте
+  yname <- titles[[1]][parTwo]
+  xl <- iris[ ,c(parOne,parTwo,5)]   #построение выборки
+  xMin <- xl[which.min(xl[ ,1]),1]   #нахождение минимального и максимальнго иксов
+  xMax <- xl[which.max(xl[ ,1]),1]
+  X <- seq(from=xMin,to=xMax,by=0.1)   #список всех иксов на карте
+  xLen <- length(X)
+  yMin <- xl[which.min(xl[ ,2]),2]   #нахождение минимального и максимального игриков
+  yMax <- xl[which.max(xl[ ,2]),2]
+  Y <- seq(from=yMin,to=yMax,by=0.1)   #список всех игриков на карте
+  yLen <-length(Y)
+  flowers <- matrix(NA,xLen,yLen)
+  positions <- matrix(NA,xLen*yLen,2)
+  optK <- determKNN_LOO(xl)   #находим оптимальное k
+  cnt <- 1
+  for (i in 1:yLen) {
+    print(Y[i])
+    for (j in 1:xLen) {
+      z <- c(X[j],Y[i])
+      positions[cnt, ] <- z   #запоминаем координаты объекта
+      flowers[j,i] <- kNN(xl,z,optK)   #находим предположительный класс методом kNN
+      cnt <- cnt+1
+    }
+  }
+  plot(positions,pch=1,bg="white",col=colors[flowers],asp=1,main="Map of kNN for optimal K",xlab=xname,ylab=yname)
+  points(iris[ ,c(3,4)],pch=21,bg=colors[iris$Species],col=colors[iris$Species],asp=1)
+}
+main()
---
+++ kNNModyfied.R
@@ -0,0 +1,101 @@
+eucDist <- function(u,v) {  #функция расстояния между парой точек
+  return (sqrt(sum((u-v)^2)))
+}
+colors <- c("setosa"="pink1","versicolor"="gold","virginica"="skyblue1")
+meanDist <- function(xl,z,k,metricFunc=eucDist) {   #
+  l <- dim(xl)[1]
+  n <- dim(xl)[2]-1
+  dist <- matrix(NA,l,2)
+  for (i in 1:l) {
+    dist[i, ] <- c(i, metricFunc(xl[i,1:n],z))  #создание списка пар (номер объекта, расстояние до z)
+  }
+  orderedDist <- dist[order(dist[, 2]), ]   #сортировка списка объектов
+  return (orderedDist[1:k,2])
+}
+subclass <- function(xl,type) {   #выбор определённого вида цветов
+  n <- dim(xl)[2]
+  return (xl[which(xl[ ,n]==type), ])
+}
+kNNMod <- function(xl,z,k) {   #функция выбора класса методов kNN
+  types <- c("setosa","versicolor","virginica")
+  n <- dim(xl)[2]-1
+  classDist <- c(0,0,0)
+  for (i in 1:3) {
+    current <- subclass(xl,types[i])   #для каждого класса получает список цветов
+    classDist[i] <- mean(meanDist(current,z,k))   #берёт среднее расстояние до ближайших k представителей класса
+  }
+  class <- types[which.min(classDist)]     #выбирает тот вид, у которого "средний" объект ближе всего к z
+  return (class)
+}
+determKNNMod_LOO <- function(xl) {   #определение оптимального k методом LOO
+  l <- dim(xl)[1]
+  n <- dim(xl)[2]-1
+  classes <- xl[ ,n+1]
+  counts <- table(classes)
+  maxK <- counts[which.min(counts)]-1   #максимальное допустимое k = минимальное число в одном классе
+  errorForK <- matrix(0.0,maxK,1)
+  for (i in 1:l) {  #перебираем каждый элемент
+    obj <- xl[i, ]
+    newXl <- xl[-i, ]  #убираем его из выборки
+    z <- obj[1:n]
+    classes <- c("setosa"=0,"versicolor"=0,"virginica"=0)
+    dist <- matrix(NA,3,maxK)
+    for (j in 1:3) {
+      class <- subclass(newXl,names(classes[j]))   #для каждого класса получает список цветов
+      dist[j, ] <- meanDist(class,z,maxK)   #берёт среднее расстояние до всех представителей класса
+    }
+    print(i)
+    for (k in 1:maxK) {   #перебираем значение k
+      for (j in 1:3) {
+        classes[j] <- classes[j]+dist[j,k]   #в каждый класс добавляется расстояние до k-го объекта класса
+      }
+      class <- names(which.min(classes))   #выбирается класс с наименьшим расстоянием
+      if (class!=obj[n+1]) {   #если произошла ошибка классификации
+        errorForK[k]=errorForK[k]+1   #то увиличиваем для данного k значение ошибки
+      }
+    }
+  }
+  LOO <- matrix(NA,maxK,2)
+  for (k in 1:maxK) {
+    LOO[k, ] <- c(k,errorForK[k])   #строим массив пар (номер, число ошибок)
+  }
+  optK <- which.min(errorForK)
+  plot(LOO,pch=1,type="l",col="blue",xlab="k",ylab="LOO(k)",main="Find optimal value of k for kNNMod with LOO-algo")
+  points(optK,LOO[optK,2],pch=21,bg="red",col="red")
+  res <- paste("optK=",optK)   #готовит подпись для графика
+  text(optK+2,LOO[optK,2]+0.05,labels=res)
+  return (optK)
+}
+
+main <- function() {
+  parOne <- 3   #чтобы можно было легко поменять параметры
+  parTwo <- 4
+  titles <- dimnames(iris)[2]
+  xname <- titles[[1]][parOne]   #названия для подписей на карте
+  yname <- titles[[1]][parTwo]
+  xl <- iris[ ,c(parOne,parTwo,5)]   #построение выборки
+  xMin <- xl[which.min(xl[ ,1]),1]   #нахождение минимального и максимальнго иксов
+  xMax <- xl[which.max(xl[ ,1]),1]
+  X <- seq(from=xMin,to=xMax,by=0.1)   #список всех иксов на карте
+  xLen <- length(X)
+  yMin <- xl[which.min(xl[ ,2]),2]   #нахождение минимального и максимального игриков
+  yMax <- xl[which.max(xl[ ,2]),2]
+  Y <- seq(from=yMin,to=yMax,by=0.1)   #список всех игриков на карте
+  yLen <-length(Y)
+  flowers <- matrix(NA,xLen,yLen)
+  positions <- matrix(NA,xLen*yLen,2)
+  optK <- determKNNMod_LOO(xl)   #находим оптимальное k
+  cnt <- 1
+  for (i in 1:yLen) {
+    print(Y[i])
+    for (j in 1:xLen) {
+      z <- c(X[j],Y[i])
+      positions[cnt, ] <- z   #запоминаем координаты объекта
+      flowers[j,i] <- kNNMod(xl,z,optK)   #находим предположительный класс методом kNN
+      cnt <- cnt+1
+    }
+  }
+  plot(positions,pch=1,bg="white",col=colors[flowers],asp=1,main="Map of kNNMod for optimal K",xlab=xname,ylab=yname)
+  points(iris[ ,c(3,4)],pch=21,bg=colors[iris$Species],col=colors[iris$Species],asp=1)
+}
+main()
