e9de6f4dbc86c559d570903336623a55e8f30f05
Inc1ementia <71116508+Inc1ementia@users.noreply.github.com>
2020-09-18 08:12:46 +0300

    Update kNN.R

--- kNN.R
+++ kNN.R
@@ -1,81 +1,81 @@
-eucDist <- function(u,v) {  #ôóíêöèÿ ðàññòîÿíèÿ ìåæäó ïàðîé òî÷åê
+eucDist <- function(u,v) {  #Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã°Ã Ã±Ã±Ã²Ã®Ã¿Ã­Ã¨Ã¿ Ã¬Ã¥Ã¦Ã¤Ã³ Ã¯Ã Ã°Ã®Ã© Ã²Ã®Ã·Ã¥Ãª
   return (sqrt(sum((u-v)^2)))
 }
 colors <- c("setosa"="pink1","versicolor"="gold","virginica"="skyblue1")
-sortObjbyDist <- function(xl,z,metricFunc=eucDist) {  #ôóíêöèÿ ñîðòèðîâêè ìàññèâà ïî ðàññòîÿíèþ äî z
+sortObjbyDist <- function(xl,z,metricFunc=eucDist) {  #Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã±Ã®Ã°Ã²Ã¨Ã°Ã®Ã¢ÃªÃ¨ Ã¬Ã Ã±Ã±Ã¨Ã¢Ã  Ã¯Ã® Ã°Ã Ã±Ã±Ã²Ã®Ã¿Ã­Ã¨Ã¾ Ã¤Ã®  z
   l <- dim(xl)[1]
   n <- dim(xl)[2]-1
   dist <- matrix(NA,l,2)
   for (i in 1:l) {
-    dist[i, ] <- c(i, metricFunc(xl[i,1:n],z))  #ñîçäàíèå ñïèñêà ïàð (íîìåð îáúåêòà, ðàññòîÿíèå äî z)
+    dist[i, ] <- c(i, metricFunc(xl[i,1:n],z))  #Ã±Ã®Ã§Ã¤Ã Ã­Ã¨Ã¥ Ã±Ã¯Ã¨Ã±ÃªÃ  Ã¯Ã Ã° (Ã­Ã®Ã¬Ã¥Ã° Ã®Ã¡ÃºÃ¥ÃªÃ²Ã , Ã°Ã Ã±Ã±Ã²Ã®Ã¿Ã­Ã¨Ã¥ Ã¤Ã® z)
   }
-  orderedXl <- xl[order(dist[ ,2]), ]   #ñîðòèðîâêà ñïèñêà îáúåêòîâ
+  orderedXl <- xl[order(dist[ ,2]), ]   #Ã±Ã®Ã°Ã²Ã¨Ã°Ã®Ã¢ÃªÃ  Ã±Ã¯Ã¨Ã±ÃªÃ  Ã®Ã¡ÃºÃ¥ÃªÃ²Ã®Ã¢
   return (orderedXl)
 }
-kNN <- function(xl,z,k) {   #ôóíêöèÿ âûáîðà êëàññà ìåòîäîâ kNN
+kNN <- function(xl,z,k) {   #Ã´Ã³Ã­ÃªÃ¶Ã¨Ã¿ Ã¢Ã»Ã¡Ã®Ã°Ã  ÃªÃ«Ã Ã±Ã±Ã  Ã¬Ã¥Ã²Ã®Ã¤Ã®Ã¢ kNN
   orderedXl <- sortObjbyDist(xl,z)
   n <- dim(orderedXl)[2]-1
-  classes <- orderedXl[1:k,n+1]   #ïîëó÷àåò ñïèñîê âèäîâ äëÿ áëèæàéøèõ k îáúåêòîâ
-  counts <- table(classes)   #ñòðîèòü èç íèõ òàáëèöó êîëè÷åñòâî öâåòîâ êàæäîãî âèäà
-  class <- names(which.max(counts))   #âûáèðàåò òîò âèä, ó êîòîðîãî áîëüøå âñåãî ïðåäñòàâèòåëåé
+  classes <- orderedXl[1:k,n+1]   #Ã¯Ã®Ã«Ã³Ã·Ã Ã¥Ã² Ã±Ã¯Ã¨Ã±Ã®Ãª Ã¢Ã¨Ã¤Ã®Ã¢ Ã¤Ã«Ã¿ Ã¡Ã«Ã¨Ã¦Ã Ã©Ã¸Ã¨Ãµ k Ã®Ã¡ÃºÃ¥ÃªÃ²Ã®Ã¢
+  counts <- table(classes)   #Ã±Ã²Ã°Ã®Ã¨Ã²Ã¼ Ã¨Ã§ Ã­Ã¨Ãµ Ã²Ã Ã¡Ã«Ã¨Ã¶Ã³ ÃªÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã¶Ã¢Ã¥Ã²Ã®Ã¢ ÃªÃ Ã¦Ã¤Ã®Ã£Ã® Ã¢Ã¨Ã¤Ã 
+  class <- names(which.max(counts))   #Ã¢Ã»Ã¡Ã¨Ã°Ã Ã¥Ã² Ã²Ã®Ã² Ã¢Ã¨Ã¤, Ã³ ÃªÃ®Ã²Ã®Ã°Ã®Ã£Ã® Ã¡Ã®Ã«Ã¼Ã¸Ã¥ Ã¢Ã±Ã¥Ã£Ã® Ã¯Ã°Ã¥Ã¤Ã±Ã²Ã Ã¢Ã¨Ã²Ã¥Ã«Ã¥Ã©
   return (class)
 }
-determKNN_LOO <- function(xl) {   #îïðåäåëåíèå îïòèìàëüíîãî k ìåòîäîì LOO
+determKNN_LOO <- function(xl) {   #Ã®Ã¯Ã°Ã¥Ã¤Ã¥Ã«Ã¥Ã­Ã¨Ã¥ Ã®Ã¯Ã²Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã£Ã® k Ã¬Ã¥Ã²Ã®Ã¤Ã®Ã¬ LOO
   l <- dim(xl)[1]
   n <- dim(xl)[2]-1
   maxK <- l-1
   errorForK <- matrix(0.0,maxK,1)
-  for (i in 1:l) {  #ïåðåáèðàåì êàæäûé ýëåìåíò
+  for (i in 1:l) {  #Ã¯Ã¥Ã°Ã¥Ã¡Ã¨Ã°Ã Ã¥Ã¬ ÃªÃ Ã¦Ã¤Ã»Ã© Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã²
     obj <- xl[i, ]
-    newXl <- xl[-i, ]  #óáèðàåì åãî èç âûáîðêè
+    newXl <- xl[-i, ]  #Ã³Ã¡Ã¨Ã°Ã Ã¥Ã¬ Ã¥Ã£Ã® Ã¨Ã§ Ã¢Ã»Ã¡Ã®Ã°ÃªÃ¨
     z <- obj[1:n]
-    orderedXl <- sortObjbyDist(newXl,z)  #íàõîäèì ðàññòîÿíèÿ äî âñåõ îáúåêòîâ
+    orderedXl <- sortObjbyDist(newXl,z)  #Ã­Ã ÃµÃ®Ã¤Ã¨Ã¬ Ã°Ã Ã±Ã±Ã²Ã®Ã¿Ã­Ã¨Ã¿ Ã¤Ã® Ã¢Ã±Ã¥Ãµ Ã®Ã¡ÃºÃ¥ÃªÃ²Ã®Ã¢
     classes <- c("setosa"=0,"versicolor"=0,"virginica"=0)
     print(i)
-    for (k in 1:maxK) {   #ïåðåáèðàåì çíà÷åíèå k
-      classes[orderedXl[k,n+1]] <- classes[orderedXl[k,n+1]]+1  #äîáàâëÿåì k-ûé ýëåìåíò â ñâîþ ãðóïïó
-      class <- names(which.max(classes))  #âûáèðàåò òîò âèä, ó êîòîðîãî áîëüøå âñåãî ïðåäñòàâèòåëåé
-      if (class!=obj[n+1]) {   #åñëè ïðîèçîøëà îøèáêà êëàññèôèêàöèè
-        errorForK[k]=errorForK[k]+1   #òî óâèëè÷èâàåì äëÿ äàííîãî k çíà÷åíèå îøèáêè
+    for (k in 1:maxK) {   #Ã¯Ã¥Ã°Ã¥Ã¡Ã¨Ã°Ã Ã¥Ã¬ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ k
+      classes[orderedXl[k,n+1]] <- classes[orderedXl[k,n+1]]+1  #Ã¤Ã®Ã¡Ã Ã¢Ã«Ã¿Ã¥Ã¬ k-Ã»Ã© Ã½Ã«Ã¥Ã¬Ã¥Ã­Ã² Ã¢ Ã±Ã¢Ã®Ã¾ Ã£Ã°Ã³Ã¯Ã¯Ã³
+      class <- names(which.max(classes))  #Ã¢Ã»Ã¡Ã¨Ã°Ã Ã¥Ã² Ã²Ã®Ã² Ã¢Ã¨Ã¤, Ã³ ÃªÃ®Ã²Ã®Ã°Ã®Ã£Ã® Ã¡Ã®Ã«Ã¼Ã¸Ã¥ Ã¢Ã±Ã¥Ã£Ã® Ã¯Ã°Ã¥Ã¤Ã±Ã²Ã Ã¢Ã¨Ã²Ã¥Ã«Ã¥Ã©
+      if (class!=obj[n+1]) {   #Ã¥Ã±Ã«Ã¨ Ã¯Ã°Ã®Ã¨Ã§Ã®Ã¸Ã«Ã  Ã®Ã¸Ã¨Ã¡ÃªÃ  ÃªÃ«Ã Ã±Ã±Ã¨Ã´Ã¨ÃªÃ Ã¶Ã¨Ã¨
+        errorForK[k]=errorForK[k]+1   #Ã²Ã® Ã³Ã¢Ã¨Ã«Ã¨Ã·Ã¨Ã¢Ã Ã¥Ã¬ Ã¤Ã«Ã¿ Ã¤Ã Ã­Ã­Ã®Ã£Ã® k Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥ Ã®Ã¸Ã¨Ã¡ÃªÃ¨
       }
     }
   }
   LOO <- matrix(NA,maxK,2)
   for (k in 1:maxK) {
-    LOO[k, ] <- c(k,errorForK[k])   #ñòðîèì ìàññèâ ïàð (íîìåð, ÷èñëî îøèáîê)
+    LOO[k, ] <- c(k,errorForK[k])   #Ã±Ã²Ã°Ã®Ã¨Ã¬ Ã¬Ã Ã±Ã±Ã¨Ã¢ Ã¯Ã Ã° (Ã­Ã®Ã¬Ã¥Ã°, Ã·Ã¨Ã±Ã«Ã® Ã®Ã¸Ã¨Ã¡Ã®Ãª)
   }
-  optK <- which.min(errorForK)  #âûáèðàåì îïòèìàëüíîå k
+  optK <- which.min(errorForK)  #Ã¢Ã»Ã¡Ã¨Ã°Ã Ã¥Ã¬ Ã®Ã¯Ã²Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã¥ k
   plot(LOO,pch=1,type="l",col="blue",xlab="k",ylab="LOO(k)",main="Find optimal value of k for kNN with LOO-algo")
   points(optK,LOO[optK,2],pch=21,bg="red",col="red")
-  res <- paste("optK=",optK)   #ãîòîâèò ïîäïèñü äëÿ ãðàôèêà
+  res <- paste("optK=",optK)   #Ã£Ã®Ã²Ã®Ã¢Ã¨Ã² Ã¯Ã®Ã¤Ã¯Ã¨Ã±Ã¼ Ã¤Ã«Ã¿ Ã£Ã°Ã Ã´Ã¨ÃªÃ 
   text(optK,LOO[optK,2]+7,labels=res)
   return (optK)
 }
 main <- function() {
-  parOne <- 3   #÷òîáû ìîæíî áûëî ëåãêî ïîìåíÿòü ïàðàìåòðû
+  parOne <- 3   #Ã·Ã²Ã®Ã¡Ã» Ã¬Ã®Ã¦Ã­Ã® Ã¡Ã»Ã«Ã® Ã«Ã¥Ã£ÃªÃ® Ã¯Ã®Ã¬Ã¥Ã­Ã¿Ã²Ã¼ Ã¯Ã Ã°Ã Ã¬Ã¥Ã²Ã°Ã»
   parTwo <- 4
   titles <- dimnames(iris)[2]
-  xname <- titles[[1]][parOne]   #íàçâàíèÿ äëÿ ïîäïèñåé íà êàðòå
+  xname <- titles[[1]][parOne]   #Ã­Ã Ã§Ã¢Ã Ã­Ã¨Ã¿ Ã¤Ã«Ã¿ Ã¯Ã®Ã¤Ã¯Ã¨Ã±Ã¥Ã© Ã­Ã  ÃªÃ Ã°Ã²Ã¥
   yname <- titles[[1]][parTwo]
-  xl <- iris[ ,c(parOne,parTwo,5)]   #ïîñòðîåíèå âûáîðêè
-  xMin <- xl[which.min(xl[ ,1]),1]   #íàõîæäåíèå ìèíèìàëüíîãî è ìàêñèìàëüíãî èêñîâ
+  xl <- iris[ ,c(parOne,parTwo,5)]   #Ã¯Ã®Ã±Ã²Ã°Ã®Ã¥Ã­Ã¨Ã¥ Ã¢Ã»Ã¡Ã®Ã°ÃªÃ¨
+  xMin <- xl[which.min(xl[ ,1]),1]   #Ã­Ã ÃµÃ®Ã¦Ã¤Ã¥Ã­Ã¨Ã¥ Ã¬Ã¨Ã­Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã£Ã® Ã¨ Ã¬Ã ÃªÃ±Ã¨Ã¬Ã Ã«Ã¼Ã­Ã£Ã® Ã¨ÃªÃ±Ã®Ã¢
   xMax <- xl[which.max(xl[ ,1]),1]
-  X <- seq(from=xMin,to=xMax,by=0.1)   #ñïèñîê âñåõ èêñîâ íà êàðòå
+  X <- seq(from=xMin,to=xMax,by=0.1)   #Ã±Ã¯Ã¨Ã±Ã®Ãª Ã¢Ã±Ã¥Ãµ Ã¨ÃªÃ±Ã®Ã¢ Ã­Ã  ÃªÃ Ã°Ã²Ã¥
   xLen <- length(X)
-  yMin <- xl[which.min(xl[ ,2]),2]   #íàõîæäåíèå ìèíèìàëüíîãî è ìàêñèìàëüíîãî èãðèêîâ
+  yMin <- xl[which.min(xl[ ,2]),2]   #Ã­Ã ÃµÃ®Ã¦Ã¤Ã¥Ã­Ã¨Ã¥ Ã¬Ã¨Ã­Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã£Ã® Ã¨ Ã¬Ã ÃªÃ±Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã£Ã® Ã¨Ã£Ã°Ã¨ÃªÃ®Ã¢
   yMax <- xl[which.max(xl[ ,2]),2]
-  Y <- seq(from=yMin,to=yMax,by=0.1)   #ñïèñîê âñåõ èãðèêîâ íà êàðòå
+  Y <- seq(from=yMin,to=yMax,by=0.1)   #Ã±Ã¯Ã¨Ã±Ã®Ãª Ã¢Ã±Ã¥Ãµ Ã¨Ã£Ã°Ã¨ÃªÃ®Ã¢ Ã­Ã  ÃªÃ Ã°Ã²Ã¥
   yLen <-length(Y)
   flowers <- matrix(NA,xLen,yLen)
   positions <- matrix(NA,xLen*yLen,2)
-  optK <- determKNN_LOO(xl)   #íàõîäèì îïòèìàëüíîå k
+  optK <- determKNN_LOO(xl)   #Ã­Ã ÃµÃ®Ã¤Ã¨Ã¬ Ã®Ã¯Ã²Ã¨Ã¬Ã Ã«Ã¼Ã­Ã®Ã¥ k
   cnt <- 1
   for (i in 1:yLen) {
     print(Y[i])
     for (j in 1:xLen) {
       z <- c(X[j],Y[i])
-      positions[cnt, ] <- z   #çàïîìèíàåì êîîðäèíàòû îáúåêòà
-      flowers[j,i] <- kNN(xl,z,optK)   #íàõîäèì ïðåäïîëîæèòåëüíûé êëàññ ìåòîäîì kNN
+      positions[cnt, ] <- z   #Ã§Ã Ã¯Ã®Ã¬Ã¨Ã­Ã Ã¥Ã¬ ÃªÃ®Ã®Ã°Ã¤Ã¨Ã­Ã Ã²Ã» Ã®Ã¡ÃºÃ¥ÃªÃ²Ã 
+      flowers[j,i] <- kNN(xl,z,optK)   #Ã­Ã ÃµÃ®Ã¤Ã¨Ã¬ Ã¯Ã°Ã¥Ã¤Ã¯Ã®Ã«Ã®Ã¦Ã¨Ã²Ã¥Ã«Ã¼Ã­Ã»Ã© ÃªÃ«Ã Ã±Ã± Ã¬Ã¥Ã²Ã®Ã¤Ã®Ã¬ kNN
       cnt <- cnt+1
     }
   }
